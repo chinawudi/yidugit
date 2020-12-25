346e2a06f7e58381392c464746d0e73bc479a450
chinawudi <2117009169@qq.com>
2020-12-25 14:49:39 +0800

    一度测试git1

---
+++ src/com/yidu/Test/test.java
@@ -0,0 +1,13 @@
+package com.yidu.Test;
+
+/**
+ * @author hzh
+ * @version 1.0
+ * @descripton:
+ * @date 2020/12/25 17:05
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
